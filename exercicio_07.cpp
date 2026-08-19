43fc89e5a2aa445c81814545c7b770b9bfd08f2a
theroven79-droid <theroven79@gmail.com>
2026-08-19 11:07:07 -0300

    uptade: att atividade

--- exercicio_07.cpp
+++ exercicio_07.cpp
@@ -8,26 +8,13 @@ int main()
     char letra;
     cout << "digite uma letra: ";
     cin >> letra;
-    char letramin = tolower(letra);
-    if (letramin == 'a' || letramin == 'e' || letramin == 'i' || letramin == 'o' || letramin == 'u')
+    if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' || letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U')
     {
-        cout << "letra: '" << letramin << "' é vogal" << endl;
+        cout << "é vogal" << endl;
     }
     else
     {
-        cout << "letra: '" << letramin << "' é consoante" << endl;
+        cout << "é consoante" << endl;
     }
-    /* cout << "" << endl;
-     for (char letra2 = 97; letra2 < 123; letra2++)
-     {
-         if (letra2 == 'a' || letra2 == 'e' || letra2 == 'i' || letra2 == 'o' || letra2 == 'u')
-         {
-             cout << "letra: '" << letra2 << "' é vogal" << endl;
-         }
-         else
-         {
-             cout << "letra: '" << letra2 << "' é consoante" << endl;
-         }
-     }*/
     return 0;
 }
